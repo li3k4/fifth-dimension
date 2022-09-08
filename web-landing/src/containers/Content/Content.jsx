764fa0f65e1345be95c1a73f7082721e2c8a9136
Liza <83118013+li3k4@users.noreply.github.com>
2022-09-08 18:41:56 +0200

    feat(new design): web-landing

--- web-landing/src/containers/Content/Content.jsx
+++ web-landing/src/containers/Content/Content.jsx
@@ -4,60 +4,72 @@ import PrivacyPolicy from "../PrivacyPolicy";
 
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
-import icon from "../../resources/logo_black.svg";
-import left from "../../resources/left.png";
-import right from "../../resources/right.png";
+import logo from "../../resources/logo_black.svg";
+import main from "../../resources/img.png";
 import UserAgreement from "../UserAgreement";
 import MainApiConstants from "../../constants/api/main.api";
 
 const Content = () => {
   return (
     <div className={styles["container"]}>
-      <div className={styles["content"]}>
-        <div className={styles["txt-icon"]}>
-          <div className={styles["icon"]}>
-            <img src={icon} />
-          </div>
-          <div className={styles["item text"]}>
-            <h1>
-              “Пятое измерение” - практики <br />
-              способные изменить твою <br />
-              жизнь к лучшему
-            </h1>
-            <h2>
-              Саморазвивайся в обасти здоровье, любовь,<br />
-              отношения, деньги, концентрация,<br />
-              предназначение. Здесь ты найдёшь  <br />
-              для себя Свои личные ключи к Счастью. <br />
-              <br />
-            </h2>
-          </div>
-          <a href={`${MainApiConstants.main_server}/data/app/app.apk`} download>
-            <button
-              className={styles["btn"]}
-              onClick={async () => {
-                /*const resp = await fetch(
+      <div className={styles["item-1"]}>
+        <img className={styles["item-1-img"]} src={main} />
+      </div>
+      <div className={styles["item-2"]}>
+        <div className={styles["item-logo"]}>
+          <img src={logo} />
+        </div>
+
+        <div className={styles["item-text"]}>
+          <h1>
+            “Пятое измерение” - практики, <br />
+            способные изменить твою <br />
+            жизнь к лучшему
+          </h1>
+          <h2>
+            Саморазвивайся в области здоровья, любви, отношений, денег,
+            концентрации, предназначения. Здесь ты найдешь свои личные ключи к
+            Счастью.
+          </h2>
+        </div>
+        <a href={`${MainApiConstants.main_server}/data/app/app.apk`} download>
+          <button
+            className={styles["btn"]}
+            onClick={async () => {
+              /*const resp = await fetch(
                   'http://localhost:5000/api/user/download/app',
                   {
                     method: "GET"
                   }
                 );*/
+              //console.log(resp);
+            }}
+          >
+            {" "}
+            Скачать приложение
+          </button>
+        </a>
 
-                //console.log(resp);
-              }}
-            > Скачать приложение для Android</button>
-          </a>
-        </div>
-
-        <NavLink to="/5measurement/policy" element={PrivacyPolicy} className={styles["pp"]}>
+        <div className={styles["item-pp"]}>
+        <NavLink
+          to="/5measurement/policy"
+          element={PrivacyPolicy}
+          className={styles["pp"]}
+        >
           {" "}
           Политика конфиденциальности{" "}
         </NavLink>
-        <NavLink to="/5measurement/agreement" element={UserAgreement} className={styles["pp2"]}>
+        <NavLink
+          to="/5measurement/agreement"
+          element={UserAgreement}
+          className={styles["pp"]}
+        >
           {" "}
           Пользовательское соглашение{" "}
         </NavLink>
       </div>
+      </div>
+
     </div>
   );
 };
